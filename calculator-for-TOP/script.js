22161febcb681de21c8e1ad293f39133f94cd193
hammichalkovic <123972691+hammichalkovic@users.noreply.github.com>
2023-10-09 13:05:05 +0300

    Asign operands display events

--- calculator-for-TOP/script.js
+++ calculator-for-TOP/script.js
@@ -1,4 +1,70 @@
-let operator = '';
+let operator = '',
+    inputField = document.querySelector('.display'),
+    button1 = document.querySelector('.one'),
+    button2 = document.querySelector('.two'),
+    button3 = document.querySelector('.three'),
+    button4 = document.querySelector('.four'),
+    button5 = document.querySelector('.five'),
+    button6 = document.querySelector('.six'),
+    button7 = document.querySelector('.seven'),
+    button8 = document.querySelector('.eight'),
+    button9 = document.querySelector('.nine'),
+    button0 = document.querySelector('.zero'),
+    period = document.querySelector('.period'),
+    equalSign = document.querySelector('.equal'),
+    plusSign = document.querySelector('.plus'),
+    minusSign = document.querySelector('.minus'),
+    multiplySign = document.querySelector('.multiply'),
+    divideSign = document.querySelector('divide'),
+    clearAllButton = document.querySelector('.clearall'),
+    clearEntryButton = document.querySelector('.clearentry');
+
+    inputField == 0;
+
+    button0.addEventListener('click', () => {
+        inputField.textContent += 0;
+    });
+
+    button1.addEventListener('click', () => {
+        inputField.textContent += 1;
+    });
+
+    button2.addEventListener('click', () => {
+        inputField.textContent += 2;
+    });
+
+    button3.addEventListener('click', () => {
+        inputField.textContent += 3;
+    });
+
+    button4.addEventListener('click', () => {
+        inputField.textContent += 4;
+    });
+
+    button5.addEventListener('click', () => {
+        inputField.textContent += 5;
+    });
+
+    button6.addEventListener('click', () => {
+        inputField.textContent += 6;
+    });
+
+    button7.addEventListener('click', () => {
+        inputField.textContent += 7;
+    });
+
+    button8.addEventListener('click', () => {
+        inputField.textContent += 8;
+    });
+
+    button9.addEventListener('click', () => {
+        inputField.textContent += 9;
+    });
+
+    plusSign.addEventListener('click', () => {
+        
+        alert(inputField.value);
+    });
 
 function toCalculate(num1, operator, num2) {
     switch (operator) {
