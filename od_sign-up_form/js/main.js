399b40a869edd35989cf731022b16527f91859f5
hammichalkovic <123972691+hammichalkovic@users.noreply.github.com>
2024-07-31 18:34:20 +0300

    Start styling pseudo-class events for form and writing script

---
+++ od_sign-up_form/js/main.js
@@ -0,0 +1,9 @@
+let firstName = document.getElementById('first_name'),
+    lastName = document.getElementById('last_name'),
+    email = document.getElementById('email'),
+    phoneNumber = document.getElementById('phone_number'),
+    password = document.getElementById('password'),
+    passwordConfrm = document.getElementById('confirm_password'),
+    formArr = [firstName, lastName, email, phoneNumber, password, passwordConfrm];
+
+    
