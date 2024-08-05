f8b0b8151bb4466d724462b8437cbb32bb96c548
hammichalkovic <123972691+hammichalkovic@users.noreply.github.com>
2024-08-05 17:32:50 +0300

    Write event listeners for formArr items and email input

--- od_sign-up_form/js/main.js
+++ od_sign-up_form/js/main.js
@@ -4,6 +4,48 @@ let firstName = document.getElementById('first_name'),
     phoneNumber = document.getElementById('phone_number'),
     password = document.getElementById('password'),
     passwordConfrm = document.getElementById('confirm_password'),
-    formArr = [firstName, lastName, email, phoneNumber, password, passwordConfrm];
+    formArr = [firstName, lastName, email, phoneNumber, password, passwordConfrm],
+    submit = document.getElementById('create_account');
+
+    function errorMessageSetting(item, value) {
+        item.setCustomValidity(value);
+        item.nextElementSibling.textContent = item.validationMessage;
+    }
+
+
+// submit.addEventListener('click', () => {
+
+//     formArr.forEach(item => {
+//         if (item.validity.tooShort ) {
+//             item.setCustomValidity('* Can\'t be empty');
+//         } 
+
+//         item.nextElementSibling.textContent == item.validationMessage;
+//     })
+
+// })
+
+// submit.addEventListener('click', () => {
+
+// })
+
+formArr.forEach(widget => {
 
     
+    
+    widget.addEventListener('focus', () => {
+        errorMessageSetting(widget, '');
+    })
+
+    
+
+});
+
+firstName.addEventListener('focusout', () => {
+    if (firstName.value == '') {
+        errorMessageSetting(firstName, '* Can\'t be empty');
+    } else if (firstName.validity.patternMismatch) {
+        errorMessageSetting(firstName, '* Letters only');
+    } 
+})
+
