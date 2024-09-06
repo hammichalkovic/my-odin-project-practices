54e4332840ad9086adc9f6aa1bddc3ba76cb3af7
hammichalkovic <123972691+hammichalkovic@users.noreply.github.com>
2024-09-06 18:30:31 +0300

    Write script in main.js

--- js_practice_projects/library_app/scripts/main.js
+++ js_practice_projects/library_app/scripts/main.js
@@ -1,10 +1,27 @@
-let myLibrary = [];
+let libraryBank = document.getElementById('library'),
+    libraryBankChildren = libraryBank.children,
+    myLibrary = ['asdfa', 'asdfasdf', 'asdfasdf', 'asdfasd'];
+
+   
 
 function Book() {
 
+}
+
+function createDivs() {
+    for (let i = 0; i < myLibrary.length; i++) {
+        libraryBank.appendChild(document.createElement('div'));
+        libraryBankChildren[i].textContent += myLibrary[i];
+    }
+    
+}
+
+function addDiv() {
+     libraryBank.appendChild(div)
 }
 
 function addBookToLibrary() {
     let userInput = prompt();
     myLibrary.push(userInput);
 }
+
