ef2f1a74327159f0b07d4a3b5d54de06d78f8f15
hammichalkovic <123972691+hammichalkovic@users.noreply.github.com>
2024-09-09 17:26:55 +0300

    Start writing script for btn eventListener

--- js_practice_projects/library_app/scripts/main.js
+++ js_practice_projects/library_app/scripts/main.js
@@ -1,30 +1,58 @@
 let libraryBank = document.getElementById('library'),
     libraryBankChildren = libraryBank.children,
-    myLibrary = ['asdfa', 'asdfasdf', 'asdfasdf', 'asdfasd'],
+    myLibrary = [],
     libraryItem = document.querySelector('.library__item'),
-    sample = document.querySelector('.sample');
-   
-sample.classList.add('nodisplay');
+    sample = document.querySelector('.sample'),
+    titleVal = title.value,
+    inputs = document.querySelectorAll('input'),
+    btn = document.getElementById('add');
 
-function Book() {
+    sample.classList.add('nodisplay');
 
+function toCleanValues() {
+    title.value = '';
+    author.value = '';
+    pages.value = '';
+    read.value = '';
 }
 
-function createDivs() {
-    for (let i = 0; i < myLibrary.length; i++) {
-        libraryBank.appendChild(libraryItem.cloneNode(true));
-        libraryBankChildren[i].textContent += myLibrary[i];
-    }
+
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+// function createDivs() {
+//     for (let i = 0; i < myLibrary.length; i++) {
+//         libraryBank.appendChild(libraryItem.cloneNode(true));
+//         libraryBankChildren[i].textContent += myLibrary[i];
+//     }
     
-}
+// }
 
 function addDiv() {
     //  libraryBank.appendChild(libraryItem);
      libraryBank.append(libraryItem);
 }
 
+
 function addBookToLibrary() {
-    let userInput = prompt();
-    myLibrary.push(userInput);
+    
 }
 
+btn.addEventListener('click', () => {
+    myLibrary.push(new Book(title.value, author.value, pages.value, read.value));
+
+    for (let i = 0; i < myLibrary.length; i++) {
+        libraryBank.appendChild(libraryItem.cloneNode(true));
+        libraryBankChildren[i].children[0].firstElementChild = myLibrary[i].title;
+        libraryBankChildren[i].children[1].firstElementChild = myLibrary[i].author;
+        libraryBankChildren[i].children[2].firstElementChild = myLibrary[i].pages;
+        libraryBankChildren[i].children[3].firstElementChild = myLibrary[i].read;
+    }
+
+    toCleanValues();
+})
