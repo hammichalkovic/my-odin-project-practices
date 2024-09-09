7b2ab1c3891598721eed08bc06fb8d590384e517
hammichalkovic <123972691+hammichalkovic@users.noreply.github.com>
2024-09-09 16:04:17 +0300

    Keep improving createDivs()

--- js_practice_projects/library_app/scripts/main.js
+++ js_practice_projects/library_app/scripts/main.js
@@ -1,8 +1,10 @@
 let libraryBank = document.getElementById('library'),
     libraryBankChildren = libraryBank.children,
-    myLibrary = ['asdfa', 'asdfasdf', 'asdfasdf', 'asdfasd'];
-
+    myLibrary = ['asdfa', 'asdfasdf', 'asdfasdf', 'asdfasd'],
+    libraryItem = document.querySelector('.library__item'),
+    sample = document.querySelector('.sample');
    
+sample.classList.add('nodisplay');
 
 function Book() {
 
@@ -10,14 +12,15 @@ function Book() {
 
 function createDivs() {
     for (let i = 0; i < myLibrary.length; i++) {
-        libraryBank.appendChild(document.createElement('div'));
+        libraryBank.appendChild(libraryItem.cloneNode(true));
         libraryBankChildren[i].textContent += myLibrary[i];
     }
     
 }
 
 function addDiv() {
-     libraryBank.appendChild(div)
+    //  libraryBank.appendChild(libraryItem);
+     libraryBank.append(libraryItem);
 }
 
 function addBookToLibrary() {
