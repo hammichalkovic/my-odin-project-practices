b6c1f096437801ed7e003ff4bd79f12ad2c55c76
hammichalkovic <123972691+hammichalkovic@users.noreply.github.com>
2024-09-25 16:54:47 +0300

    Restructure game object and set player/computer clicks arrays pushing script

--- js_practice_projects/tic_tac_toe/main.js
+++ js_practice_projects/tic_tac_toe/main.js
@@ -28,23 +28,63 @@ let game = createGame('Nick', 'Computer'); // try to desctructure it
 
 
 
-function extractForPlayer(num) {
+function extractValues(playerNum) {
 
-    
+    let {player, computer, gameboard} = game,
+        {name: playerName, clicks: playerClicks, scoreIncrement: playerScoreIncrement} = player,
+        {name: computerName, clicks: computerClicks, scoreIncrement: computerScoreIncrement} = computer;
 
-    // if (game.player.clicks.length > 2) {
-        // if (game.player.clicks.slice(game.player.clicks.length - 3, game.player.clicks.length)) 
-            // to use sort() and reduce() methods to create conditions for +1,+2,+3, +4 wins
-    // }
-    
-    let extractVal = game.gameboard.filter(i => i == num);
-    game.gameboard = game.gameboard.filter(i => i != num);
-    game.player.clicks = game.player.clicks.concat(extractVal);
+    let playerExtractVal = gameboard.filter(i => i == playerNum);
+    gameboard = gameboard.filter(i => i != playerNum);
+
+    let computerNum = Math.floor(Math.random() * (gameboard.length - 1) + 1),
+        computerExtractVal = gameboard.filter(i => i == gameboard[computerNum])
+        gameboard = gameboard.filter(i => i != gameboard[computerNum]);
+
+    playerClicks = player.clicks.concat(playerExtractVal);
+    computerClicks = computer.clicks.concat(computerExtractVal);
+    // cons
+    // console.log(extractVal);
+    // game.player.clicks = game.player.clicks.concat(extractVal);
     // game.player.clicks = 
     // game.player.clicks = game.player.clicks.push(extractVal);
 
+    computer = {name: computerName, clicks: computerClicks, scoreIncrement: computerScoreIncrement};
+    player = {name: playerName, clicks: playerClicks, scoreIncrement: playerScoreIncrement};
+    game = {player, computer, gameboard}
 
-    console.log(game.player.clicks);
-    console.log(game.gameboard);
+    // console.log(player.clicks);
+    console.log(playerClicks);
+    // console.log(computer.clicks);
+    console.log(computerClicks);
+    console.log(gameboard);
+    console.log(player);
+    console.log(computer);
+    console.log(game);
+
+    
+    if (playerClicks.length > 2) {
+        if (playerClicks.slice(playerClicks.length - 3, playerClicks.length)) {
+            playerClicks.sort((a, b) => a - b);
+            computerClicks.sort((a, b) => a - b);  // think about sorting and condition. it can be backware order for winning
+
+            if (playerClicks.reduce((a, b) => a + 1) == playerClicks[playerClicks.length - 1] ||
+                playerClicks.reduce((a, b) => a + 2) == playerClicks[playerClicks.length - 1] ||
+                playerClicks.reduce((a, b) => a + 3) == playerClicks[playerClicks.length - 1] ||
+                playerClicks.reduce((a, b) => a + 4) == playerClicks[playerClicks.length - 1]) {
+                    return 'You won!'
+            } else if (
+                computerClicks.reduce((a, b) => a + 1) == computerClicks[computerClicks.length - 1] ||
+                computerClicks.reduce((a, b) => a + 2) == computerClicks[computerClicks.length - 1] ||
+                computerClicks.reduce((a, b) => a + 3) == computerClicks[computerClicks.length - 1] ||
+                computerClicks.reduce((a, b) => a + 4) == computerClicks[computerClicks.length - 1]
+                ) {
+                    return 'Computer Won!'
+            }
+        }
+
+            // to use sort() and reduce() methods to create conditions for +1,+2,+3, +4 wins
+    } 
+    
 }
 
