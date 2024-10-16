f1a03ae01a9ef571867273b5035a57530ab88123
hammichalkovic <123972691+hammichalkovic@users.noreply.github.com>
2024-10-16 22:06:04 +0300

    Change Book-constructor to Book-class

--- js_practice_projects/library_app/scripts/main.js
+++ js_practice_projects/library_app/scripts/main.js
@@ -23,14 +23,21 @@ function toCleanValues() {
     read.value = '';
 }
 
-
-
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 }
+
+// function Book(title, author, pages, read) {
+//     this.title = title;
+//     this.author = author;
+//     this.pages = pages;
+//     this.read = read;
+// }
 
 
 function errorMessageSetting(item, value) {
