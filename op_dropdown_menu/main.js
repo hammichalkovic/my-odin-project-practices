64a3b3e00e9ae3c262b4743aee931d0c7bfcab5c
hammichalkovic <123972691+hammichalkovic@users.noreply.github.com>
2025-02-17 15:52:46 +0300

    Style and write script for .threedots_menu element

--- op_dropdown_menu/main.js
+++ op_dropdown_menu/main.js
@@ -1,13 +1,18 @@
 let navBody = document.querySelector(".nav_body"),
   navOpener = document.querySelector("#nav_open"),
-  navCloser = document.querySelector(".nav_close");
+  navCloser = document.querySelector(".nav_close"),
+  threeDotsMenu = document.querySelector(".threedots_menu"),
+  threeDotsToggler = document.querySelector(".threedots_toggler");
 
 navOpener.addEventListener("click", () => {
   console.log("clicked!");
-  navBody.classList.toggle("hidden");
+  navBody.classList.toggle("hidden_nav");
 });
 
 navCloser.addEventListener("click", () => {
   console.log("clicked!");
-  navBody.classList.toggle("hidden");
+  navBody.classList.toggle("hidden_nav");
+});
+threeDotsToggler.addEventListener("click", () => {
+  threeDotsMenu.classList.toggle("hidden_threedots");
 });
