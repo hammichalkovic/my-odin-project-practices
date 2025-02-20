31e66ee63c0fcfe473e078c423663a4528aab4c7
hammichalkovic <123972691+hammichalkovic@users.noreply.github.com>
2025-02-20 19:36:09 +0300

    Create an automatically spining carousel

--- op_dropdown_menu/main.js
+++ op_dropdown_menu/main.js
@@ -2,7 +2,8 @@ let navBody = document.querySelector(".nav_body"),
   navOpener = document.querySelector("#nav_open"),
   navCloser = document.querySelector(".nav_close"),
   threeDotsMenu = document.querySelector(".threedots_menu"),
-  threeDotsToggler = document.querySelector(".threedots_toggler");
+  threeDotsToggler = document.querySelector(".threedots_toggler"),
+  carouselMovingPart = document.querySelector(".carousel_moving");
 
 function toToggle(clicked, transformed, cls) {
   clicked.addEventListener("click", () => {
@@ -13,3 +14,21 @@ function toToggle(clicked, transformed, cls) {
 toToggle(navOpener, navBody, "hidden_nav");
 toToggle(navCloser, navBody, "hidden_nav");
 toToggle(threeDotsToggler, threeDotsMenu, "hidden_threedots");
+
+function toRepeatFunction(func, inteval) {
+  func();
+
+  return setInterval(func, inteval);
+}
+
+function translateCarousel() {
+  carouselMovingPart.style.cssText = "transform: translateX(-100vw);";
+  setTimeout(() => {
+    carouselMovingPart.style.cssText = "transform: translateX(-200vw);";
+  }, 4000);
+  setTimeout(() => {
+    carouselMovingPart.style.cssText = "transform: translateX(0vw);";
+  }, 8000);
+}
+
+toRepeatFunction(translateCarousel, 12000);
